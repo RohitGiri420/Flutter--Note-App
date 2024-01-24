1e741e9e1283f3156d9236b85256f4207027833b
Rohit Giri <giri420rohit@gmail.com>
2024-01-24 16:15:21 +0530

    NoteModel file is added

--- lib/Database/DbHelper.dart
+++ lib/Database/DbHelper.dart
@@ -4,33 +4,32 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DbHelper{
-
-  String TableName = "Notes_Table";
-  String NotesId ="Notes_ID";
-  String NotesTitle = "Notes_Title";
-  String NtoesDisc = "Notes_Disc";
+class DbHelper {
+  static String TableName = "Notes_Table";
+  static String NotesId = "Notes_ID";
+  static String NotesTitle = "Notes_Title";
+  static String NtoesDisc = "Notes_Disc";
 
   Database? _database;
 
-  getDb(){
-    if(_database!=null){
+  getDb() {
+    if (_database != null) {
       return _database;
-    }
-
-    else{
+    } else {
       initDB();
     }
   }
 
-
-
-
-Future<Database> initDB() async{
-  Directory directory = await getApplicationDocumentsDirectory();
-  var path = join(directory.path+"notesDb.db");
-  return openDatabase(path,version: 1,onCreate: (db, version) {
-    db.execute("CREATE TABLE $TableName($NotesId INTEGER PRIMARY KEY AUTOINCREMENT, $NotesTitle TEXT, $NtoesDisc TEXT )");
-  },);
-}
+  Future<Database> initDB() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    var path = join(directory.path + "notesDb.db");
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute(
+            "CREATE TABLE $TableName($NotesId INTEGER PRIMARY KEY AUTOINCREMENT, $NotesTitle TEXT, $NtoesDisc TEXT )");
+      },
+    );
+  }
 }
