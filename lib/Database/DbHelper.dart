a6821a15ec1bcfdd8a1a55974e46c1c8599a6457
Rohit Giri <giri420rohit@gmail.com>
2024-01-24 16:09:43 +0530

    initdb and getDb function is created

--- lib/Database/DbHelper.dart
+++ lib/Database/DbHelper.dart
@@ -1,1 +1,36 @@
-c
+import 'dart:io';
+import 'package:path/path.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DbHelper{
+
+  String TableName = "Notes_Table";
+  String NotesId ="Notes_ID";
+  String NotesTitle = "Notes_Title";
+  String NtoesDisc = "Notes_Disc";
+
+  Database? _database;
+
+  getDb(){
+    if(_database!=null){
+      return _database;
+    }
+
+    else{
+      initDB();
+    }
+  }
+
+
+
+
+Future<Database> initDB() async{
+  Directory directory = await getApplicationDocumentsDirectory();
+  var path = join(directory.path+"notesDb.db");
+  return openDatabase(path,version: 1,onCreate: (db, version) {
+    db.execute("CREATE TABLE $TableName($NotesId INTEGER PRIMARY KEY AUTOINCREMENT, $NotesTitle TEXT, $NtoesDisc TEXT )");
+  },);
+}
+}
